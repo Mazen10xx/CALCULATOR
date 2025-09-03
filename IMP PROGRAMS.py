deb022a7615204b27e754f29ef38991dfd26ec14
Mazen Ejaj Zari <mazenejaj@gmail.com>
2025-09-03 22:25:54 +0530

    Add files via upload

---
+++ IMP PROGRAMS.py
@@ -0,0 +1,9 @@
+def operators(num1,num2):
+    print("Sum of two numbers is",num1+num2)
+    print("Difference of two numbers is",num1-num2)
+    print("Product of two numbers is",num1*num2)
+    print("Quotient of two numbers is",num1/num2)
+    print("Remainder of two numbers is",num1%num2)
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+operators(num1,num2)
